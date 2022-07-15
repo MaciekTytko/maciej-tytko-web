025fbf0313ad169f66586e1113729b5264c9909a
Maciek <maciej.tytko@gmail.com>
2022-07-15 15:50:03 +0200

    About me page

---
+++ src/Pages/AboutMe/AboutMe.js
@@ -0,0 +1,45 @@
+import './AboutMe.scss'
+
+
+export default function aboutMe(){
+  return(
+    <div className='AboutMe'>
+      <Profile/>
+      <Contact/>
+      <Blog/>
+      <Maps/>
+    </div>
+  )
+}
+
+function Profile(){
+  return(
+    <div>
+
+    </div>
+  )
+}
+
+function Blog(){
+  return(
+    <div>
+      
+    </div>
+  )
+}
+
+function Contact(){
+  return(
+    <div>
+      
+    </div>
+  )
+}
+
+function Maps(){
+  return(
+    <div>
+      
+    </div>
+  )
+}
