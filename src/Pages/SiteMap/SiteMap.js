7f62a0484a1dda4c2e20bf24c2d27d9e4db40a33
Maciek <maciej.tytko@gmail.com>
2022-08-15 09:39:18 +0200

    new icon

--- src/Pages/SiteMap/SiteMap.js
+++ src/Pages/SiteMap/SiteMap.js
@@ -6,56 +6,105 @@ import ListItemButton from '@mui/material/ListItemButton';
 import ListItemIcon from '@mui/material/ListItemIcon';
 import ListItemText from '@mui/material/ListItemText';
 import Collapse from '@mui/material/Collapse';
-import InboxIcon from '@mui/icons-material/MoveToInbox';
-import DraftsIcon from '@mui/icons-material/Drafts';
-import SendIcon from '@mui/icons-material/Send';
 import ExpandLess from '@mui/icons-material/ExpandLess';
 import ExpandMore from '@mui/icons-material/ExpandMore';
 import StarBorder from '@mui/icons-material/StarBorder';
+import { useState } from 'react';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import YouTubeIcon from '@mui/icons-material/YouTube';
+import FacebookIcon from '@mui/icons-material/Facebook';
+import PictureAsPdfIcon from '@mui/icons-material/PictureAsPdf';
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+import SettingsSuggestIcon from '@mui/icons-material/SettingsSuggest';
 
 export default function SiteMap() {
+  const [open, setOpen] = useState(false);
+
+
+
+
   return (
     <div className='siteMap'>
       <Box minWidth={240}>
-      <List
-      sx={{ width: '100%', maxWidth: 360, bgcolor: 'background.paper' }}
-      component="nav"
-      aria-labelledby="nested-list-subheader"
-      subheader={
-        <ListSubheader component="div" id="nested-list-subheader">
-          Nested List Items
-        </ListSubheader>
-      }
-    >
-      <ListItemButton>
-        <ListItemIcon>
-        </ListItemIcon>
-        <ListItemText primary="Sent mail" />
-      </ListItemButton>
-      <ListItemButton>
-        <ListItemIcon>
-          <DraftsIcon />
-        </ListItemIcon>
-        <ListItemText primary="Drafts" />
-      </ListItemButton>
-      <ListItemButton onClick={handleClick}>
-        <ListItemIcon>
-          <InboxIcon />
-        </ListItemIcon>
-        <ListItemText primary="Inbox" />
-        {open ? <ExpandLess /> : <ExpandMore />}
-      </ListItemButton>
-      <Collapse in={open} timeout="auto" unmountOnExit>
-        <List component="div" disablePadding>
-          <ListItemButton sx={{ pl: 4 }}>
+        <List
+          sx={{ width: '100%', maxWidth: 400, bgcolor: 'bisque' }}
+          component="nav"
+          aria-labelledby="nested-list-subheader"
+          subheader={
+            <ListSubheader
+              component="div"
+              id="nested-list-subheader"
+              sx={{ bgcolor: 'bisque' }}
+            >
+              MENU
+            </ListSubheader>
+          }
+        >
+          <ListItemButton>
             <ListItemIcon>
-              <StarBorder />
+              <AccountCircleIcon />
             </ListItemIcon>
-            <ListItemText primary="Starred" />
+            <ListItemText primary="O mnie" />
+            {open ? <ExpandLess /> : <ExpandMore />}
           </ListItemButton>
+          <Collapse in={open} timeout="auto" unmountOnExit>
+            <List component="div" disablePadding>
+              <ListItemButton sx={{ pl: 4 }}>
+                <ListItemIcon>
+                  <StarBorder />
+                </ListItemIcon>
+                <ListItemText primary="Starred" />
+              </ListItemButton>
+            </List>
+          </Collapse>
+
+          <ListItemButton>
+            <ListItemIcon>
+              <SettingsSuggestIcon />
+            </ListItemIcon>
+            <ListItemText primary="Prace zebrane" />
+            {open ? <ExpandLess /> : <ExpandMore />}
+          </ListItemButton>
+          <Collapse in={open} timeout="auto" unmountOnExit>
+            <List component="div" disablePadding>
+              <ListItemButton sx={{ pl: 4 }}>
+                <ListItemIcon>
+                  <StarBorder />
+                </ListItemIcon>
+                <ListItemText primary="Starred" />
+              </ListItemButton>
+            </List>
+          </Collapse>
+
+          <ListItemButton>
+            <ListItemIcon>
+              <LinkedInIcon />
+            </ListItemIcon>
+            <ListItemText primary="LinkedIn (Publikacje nt. technologii)" />
+          </ListItemButton>
+
+          <ListItemButton>
+            <ListItemIcon>
+              <YouTubeIcon />
+            </ListItemIcon>
+            <ListItemText primary="YouTube (Kurs online Asix SCADA)" />
+          </ListItemButton>
+
+          <ListItemButton>
+            <ListItemIcon>
+              <FacebookIcon />
+            </ListItemIcon>
+            <ListItemText primary="Gliwickie Spotkania RPG" />
+          </ListItemButton>
+
+          <ListItemButton>
+            <ListItemIcon>
+              <PictureAsPdfIcon />
+            </ListItemIcon>
+            <ListItemText primary="Podgląd mojego CV" />
+          </ListItemButton>
+
         </List>
-      </Collapse>
-    </List>
       </Box>
     </div>
   );
