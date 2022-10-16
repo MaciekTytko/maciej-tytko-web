7d92a521be6ef69d4ca861045a6671288ff043a5
Maciek <maciej.tytko@gmail.com>
2022-10-16 22:33:29 +0200

    end menu animation

--- src/Containers/Menu/Menu.js
+++ src/Containers/Menu/Menu.js
@@ -5,6 +5,8 @@ import IconButton from '@mui/material/IconButton';
 import { NavLink, useLocation, useNavigate } from 'react-router-dom';
 import { useEffect } from 'react';
 import { useState } from 'react';
+import { CSSTransition } from 'react-transition-group';
+import { useRef } from 'react';
 
 
 export default function Menu() {
@@ -21,55 +23,83 @@ export default function Menu() {
   }, [location])
 
   return (
-      <div className='menuExpanded'>
-        < div className='menuMainPage' >
-          <Profile />
-        </div >
-      </div >
+    <>
+      <Profile show={menuExpanded} />
+      <MenuExpandedTop horizontal={menuExpanded} />
+    </>
   );
 }
 
-function Profile() {
+function Profile(props) {
   const navigator = useNavigate();
+  const nodeRef = useRef(null);
   return (
-    <div>
-      <h1 className='title'>Maciej Tytko</h1>
-      <p>
-        Jestem inżynierem i rysuję obrazki.
-        Tworzę wizualizacje fabryk z możliwością podglądnięcia ich pracy. Dla menedżerów przygotowuję stosowne raporty.
-        Po godzinach szerzę ideę grania bez prądu w stowarzyszeniu.
-        Zastrzyk endorfin zapewniają mi przejażdżki rowerowe po okolicznych lasach.
-      </p>
-      <Button variant="contained" onClick={() => navigator('/aboutme')}>O mnie</Button>
-    </div>
+    <CSSTransition
+      in={props.show}
+      nodeRef={nodeRef}
+      timeout={1000}
+      unmountOnExit={true}
+      classNames="menuExpanded">
+      <div
+        ref={nodeRef}
+        className='menuExpanded'>
+        < div className='menuMainPage' >
+          <div>
+            <h1 className='title'>Maciej Tytko</h1>
+            <p>
+              Jestem inżynierem i rysuję obrazki.
+              Tworzę wizualizacje fabryk z możliwością podglądnięcia ich pracy. Dla menedżerów przygotowuję stosowne raporty.
+              Po godzinach szerzę ideę grania bez prądu w stowarzyszeniu.
+              Zastrzyk endorfin zapewniają mi przejażdżki rowerowe po okolicznych lasach.
+            </p>
+            <Button variant="contained" onClick={() => navigator('/aboutme')}>O mnie</Button>
+          </div>
+        </div >
+      </div >
+    </CSSTransition>
   )
 }
-function MenuExpandedTop() {
-  const navigate = useNavigate();
+function MenuExpandedTop(props) {
+  const navigator = useNavigate();
   const location = useLocation();
+  const nodeRef = useRef(null);
+
   const gotoMenu = () => {
-    if (location.pathname !== '/menu') navigate('/menu');
-    else navigate('/');
+    if (location.pathname !== '/menu') navigator('/menu');
+    else navigator('/');
   };
+
   return (
-    <div className='header'>
-      <div className='logo'>
-        <img
-          className='image'
-          src={faceImage}
-          alt=""
-          width={70}
-        />
-      </div>
-      <IconButton color="secondary" onClick={gotoMenu}>
-        <NavLink to="/menu">
-          <div className='menu-button'>
-            <div className='line1'> </div>
-            <div className='line2'> </div>
-            <div className='line3'> </div>
+    <CSSTransition
+      in={!props.horizontal}
+      nodeRef={nodeRef}
+      timeout={1000}
+      classNames="header">
+      <div
+        ref={nodeRef}
+        className='header'>
+        <IconButton color="primary" onClick={gotoMenu}>
+          <NavLink to="/menu">
+            <div className='menu-button'>
+              <div className='line1'> </div>
+              <div className='line2'> </div>
+              <div className='line3'> </div>
+            </div>
+          </NavLink>
+        </IconButton>
+        <IconButton 
+              className="image"
+              color="primary" 
+              onClick={()=>navigator('/')}>
+          <div>
+            <img
+              src={faceImage}
+              alt=""
+              width={70}
+            />
           </div>
-        </NavLink>
-      </IconButton>
-    </div>
+        </IconButton>
+      </div>
+    </CSSTransition>
   )
 }
--- src/Pages/AboutMe/AboutMe.js
+++ src/Pages/AboutMe/AboutMe.js
@@ -1,52 +1,118 @@
+import { Accordion, AccordionDetails, AccordionSummary, Box, Typography } from '@mui/material'
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 import './AboutMe.scss'
 
 
-export default function aboutMe(){
-  return(
+export default function aboutMe() {
+  return (
     <div className='aboutMe'>
-      <Profile/>
-      <Contact/>
-      <Blog/>
-      <Maps/>
+      <Profile />
+      <Contact />
+      <Blog />
+      <Maps />
     </div>
   )
 }
 
-function Profile(){
-  return(
-    <div>
-      <div>
-
-      </div>
-      <div>
-        <span>Inżynier [LinkedIn]</span>
-        <span>Designer [Blog]</span>
-        <span>Fantasta [Facebook]</span>
-        <span>Pracownik [CV]</span>
-      </div>
-
+function Profile() {
+  return (
+    <div className='profile'>
+      <Box  sx={{ width: '100%', maxWidth: 1000 }}>
+        <Typography
+          variant="h1"
+          gutterBottom
+          align="center"
+        >
+          Maciej Tytko
+        </Typography>
+        <Typography
+          variant="h2"
+          align="Left"
+        >
+          O mnie
+        </Typography>
+        <Accordion>
+          <AccordionSummary
+            expandIcon={<ExpandMoreIcon />}
+            aria-controls="panel1a-content"
+            id="panel1a-header"
+          >
+            <Typography variant="h5">Inżynier</Typography>
+          </AccordionSummary>
+          <AccordionDetails>
+            <Typography>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+              malesuada lacus ex, sit amet blandit leo lobortis eget.
+            </Typography>
+          </AccordionDetails>
+        </Accordion>
+        <Accordion>
+          <AccordionSummary
+            expandIcon={<ExpandMoreIcon />}
+            aria-controls="panel1a-content"
+            id="panel1a-header"
+          >
+            <Typography variant="h5">Designer</Typography>
+          </AccordionSummary>
+          <AccordionDetails>
+            <Typography>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+              malesuada lacus ex, sit amet blandit leo lobortis eget.
+            </Typography>
+          </AccordionDetails>
+        </Accordion>
+        <Accordion>
+          <AccordionSummary
+            expandIcon={<ExpandMoreIcon />}
+            aria-controls="panel1a-content"
+            id="panel1a-header"
+          >
+            <Typography variant="h5">Prezes Ligi Znakkomitych Mistrzów Gry</Typography>
+          </AccordionSummary>
+          <AccordionDetails>
+            <Typography>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+              malesuada lacus ex, sit amet blandit leo lobortis eget.
+            </Typography>
+          </AccordionDetails>
+        </Accordion>
+        <Accordion>
+          <AccordionSummary
+            expandIcon={<ExpandMoreIcon />}
+            aria-controls="panel1a-content"
+            id="panel1a-header"
+          >
+            <Typography variant="h5">Dobry pracownik</Typography>
+          </AccordionSummary>
+          <AccordionDetails>
+            <Typography>
+              Ściągnij CV
+            </Typography>
+          </AccordionDetails>
+        </Accordion>
+      </Box>
     </div>
   )
 }
 
-function Blog(){
-  return(
+function Blog() {
+  return (
     <div>
       Blog
     </div>
   )
 }
 
-function Contact(){
-  return(
+function Contact() {
+  return (
     <div>
       Kontakt
     </div>
   )
 }
 
-function Maps(){
-  return(
+function Maps() {
+  return (
     <div>
       Mapa
     </div>
